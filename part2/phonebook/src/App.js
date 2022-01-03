f89871da24d660baf56c8c6d71a35ba07ee437c4
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-03 20:53:29 +0200

    add exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchField, setSearchField] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -16,6 +20,17 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleSearchFieldChange = (event) => {
+    setSearchField(event.target.value)
+  }
+
+  useEffect(() => {
+    const filteredAfterChangeInSearchField = persons.filter(
+      person => person.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    setFilteredPersons(filteredAfterChangeInSearchField)
+  }, [persons, searchField])
 
   const addName = (event) => {
     event.preventDefault()
@@ -38,6 +53,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        filter shown with <input value={searchField} onChange={handleSearchFieldChange}/>
+      <h2>Add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {filteredPersons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
