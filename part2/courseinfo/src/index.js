c1158076ea084398dd40f010f02264a6ee24960d
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-03 16:34:32 +0200

    add exercise 2.1

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = ({ part }) => {
+  const { name, exercises } = part
+  return (
+    <p>
+      {name} {exercises}
+    </p>    
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  const { name, parts } = course
+  return (
+    <div>
+      <Header text={name} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
