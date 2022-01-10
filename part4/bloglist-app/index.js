0bccb786be6b08ae3536221a5bd0e36284a59598
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-10 20:26:37 +0200

    add exercise 4.1

---
+++ part4/bloglist-app/index.js
@@ -0,0 +1,48 @@
+// const http = require('http')
+require('dotenv').config()
+const morgan = require('morgan')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+morgan.token('request-body', (req) => JSON.stringify(req.body))
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms :res[content-length] :request-body'))
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
