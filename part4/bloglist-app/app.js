72a13c7f513ab8ef6094f5c3a55cb261d3e24093
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-10 21:10:16 +0200

    add exercise 4.2

---
+++ part4/bloglist-app/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+morgan.token('request-body', (req) => JSON.stringify(req.body))
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms :res[content-length] :request-body'))
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/bloglist-app/index.js
+++ part4/bloglist-app/index.js
@@ -1,48 +1,10 @@
-// const http = require('http')
-const morgan = require('morgan')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const logger = require('./utils/logger')
+const http = require('http')
+const app = require('./app')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-
-morgan.token('request-body', (req) => JSON.stringify(req.body))
-
-app.use(cors())
-app.use(express.json())
-app.use(morgan(':method :url :status :response-time ms :res[content-length] :request-body'))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
