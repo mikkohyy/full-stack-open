265a244028b755959a83dde7d82629e22cf9ff57
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-11 20:26:51 +0200

    add exercise 4.4

--- part4/bloglist-app/utils/list_helper.js
+++ part4/bloglist-app/utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
   return blogs
 }
 
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((sumOfLikes, blog) => {
+    return sumOfLikes + blog.likes
+  }, 0)
+  return totalLikes
+}
+
 module.exports =  {
-  dummy
+  dummy,
+  totalLikes
 }
