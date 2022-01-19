a9b20dc45d5750c6d80a0b8354d368f83fad0e44
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-19 09:59:57 +0200

    add exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Button from './components/Button'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -16,6 +17,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,13 +32,22 @@ const App = () => {
       const loggedInUser = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(loggedInUser)
+      )
+
       setUser(loggedInUser)
       setUsername('')
       setPassword('')
-      console.log(user)
     } catch (expection) {
       console.log('Wrong credentials')
     }
+  }
+
+  const handleLogOut = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -49,7 +67,7 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{`${user.name} logged in`}</p>
+        <p>{`${user.name} logged in`} <Button text="logout" onClick={handleLogOut} /></p>
         {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
       </div>
     )
