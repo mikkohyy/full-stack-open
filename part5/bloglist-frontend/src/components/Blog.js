e89b82921c072b19ea52ce560bfb07cf1ec9e2a8
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-19 21:26:33 +0200

    add exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderColor: 'lightgray',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [viewText, setViewText] = useState(true)
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false)
+
+  const buttonText = viewText ? 'view' : 'hide'
+  const showWhenVisible = { display: showAdditionalInfo ? '' : 'none' }
+
+  const viewHideAdditionalInfo = () => {
+    setViewText(!viewText)
+    setShowAdditionalInfo(!showAdditionalInfo)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        "{blog.title}" by {blog.author} <Button text={buttonText} onClick={viewHideAdditionalInfo} />
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}<br/>
+        {blog.likes} <Button text="like" /><br/>
+        {blog.user.name}<br/>
+      </div>
+    </div>
+  )
+}  
 
 export default Blog
