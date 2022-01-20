898c7523d8b47fb248515477c6621be4c22fef27
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-20 20:43:35 +0200

    add exercise 5.8

--- part4/bloglist-app/controllers/blogs.js
+++ part4/bloglist-app/controllers/blogs.js
@@ -57,11 +57,13 @@ blogsRouter.put('/:id', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   const updatedBlog = await Blog
     .findByIdAndUpdate(request.params.id, blogToBeModified, { new: true })
+    .populate('user', { username: 1, name: 1 })
 
   response.json(updatedBlog)
 })
