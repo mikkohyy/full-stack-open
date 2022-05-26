8bb20a29a7743742bf9396291e88dabb9a02e973
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-05-26 12:54:31 +0300

    add exercise 6.1

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,23 +1,59 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
+  bad: 0,
+  all: 0,
+  average: 0,
+  positive: 0
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
+    case 'GOOD': {
+      const newState = { ...state, good: state.good + 1, all: state.all + 1 }
+      const allUpdatedNewState = {
+        ...newState,
+        average: getAverage(newState),
+        positive: getPositivesFraction(newState)
+      }
+      return allUpdatedNewState
+    } case 'OK': {
+      const newState = { ...state, ok: state.ok + 1, all: state.all + 1 }
+      const allUpdatedNewState = {
+        ...newState,
+        average: getAverage(newState),
+        positive: getPositivesFraction(newState)
+      }
+      return allUpdatedNewState
+    } case 'BAD': {
+      const newState = { ...state, bad: state.bad + 1, all: state.all + 1 }
+      const allUpdatedNewState = {
+        ...newState,
+        average: getAverage(newState),
+        positive: getPositivesFraction(newState)
+      }
+      return allUpdatedNewState
+    } case 'ZERO':
       return state
     default: return state
   }
   
+}
+
+const getAverage = (source) => {
+  const score = (1 * source.good) + (-1 * source.bad)
+  const average = score / source.all
+
+  const roundedAverage = Math.round(average * 100) / 100
+
+  return roundedAverage;
+}
+
+const getPositivesFraction = (source) => {
+  const positivesFraction = source.good / source.all * 100
+  const roundedPositivesFraction = Math.round(positivesFraction * 100) / 100
+
+  return roundedPositivesFraction
 }
 
 export default counterReducer
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -5,7 +5,10 @@ describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
     ok: 0,
-    bad: 0
+    bad: 0,
+    all: 0,
+    average: 0,
+    positive: 0
   }
 
   test('should return a proper initial state when called with undefined state', () => {
@@ -29,7 +32,106 @@ describe('unicafe reducer', () => {
     expect(newState).toEqual({
       good: 1,
       ok: 0,
-      bad: 0
+      bad: 0,
+      all: 1,
+      average: 1,
+      positive: 100
     })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+      all: 1,
+      average: 0,
+      positive: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+      all: 1,
+      average: -1,
+      positive: 0
+    })
+  })
+
+  test('multiple evaluations increase', () => {
+    const firstAction = {
+      type: 'GOOD'
+    }
+    const firstState = initialState
+    deepFreeze(firstState)
+    const firstNewState = counterReducer(firstState, firstAction)
+    
+    const secondAction = {
+      type: 'GOOD'
+    }
+
+    const secondState = firstNewState
+    deepFreeze(secondState)
+    const secondNewState = counterReducer(secondState, secondAction)
+    
+    const thirdAction = {
+      type: 'OK'
+    }
+    const thirdState = secondNewState
+    deepFreeze(thirdState)
+    const thirdNewState = counterReducer(thirdState, thirdAction)
+    
+    expect(thirdNewState).toEqual({
+      good: 2,
+      ok: 1,
+      bad: 0,
+      all: 3,
+      average: 0.67,
+      positive: 66.67
+    })    
+  })
+
+  test('all, average and positive are counted right', () => {
+    const action = {
+      type: 'OK'
+    }
+
+    const state = {
+      good: 6,
+      ok: 2,
+      bad: 1,
+      all: 9,
+      average: 0.56,
+      positive: 66.67
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+  
+    expect(newState).toEqual({
+      good: 6,
+      ok: 3,
+      bad: 1,
+      all: 10,
+      average: 0.5,
+      positive: 60
+    })    
   })
 })
