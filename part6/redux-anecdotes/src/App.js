5a9c9de125ef28f4cd5b29f2a88e26af7c61bf2f
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-05-30 19:57:01 +0300

    add exercise 6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Anecdotes from './components/AnecdoteList'
 import Form from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   const visible = useSelector(state => state.notification.visible)
 
   return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -18,26 +18,19 @@ export const addAnecdote = (content) => {
   }
 }
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
+export const setAnecdotes = (anecdotes) => {
   return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
+  type: "SET_ANECDOTES",
+    data: {
+      anecdotes: anecdotes
+    }
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const initialState = []
 
 const anecdoteReducer = (state = initialState, action) => {
   console.log('action', action)
@@ -50,6 +43,9 @@ const anecdoteReducer = (state = initialState, action) => {
     case 'ADD_ANECDOTE': {
       const updatedAnecdotes = state.concat(action.data)
       return updatedAnecdotes
+    } case 'SET_ANECDOTES' : {
+      const currentAnecdotes = action.data.anecdotes;
+      return currentAnecdotes
     }
     default:
       return state
