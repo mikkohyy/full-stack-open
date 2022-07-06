403cd2cd42888bc8179d97448a1ddf9b5e489016
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-07-06 12:52:54 +0300

    add exercise 10.9 and 10.10

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,10 +1,24 @@
 import SignInForm from "./SignInForm";
 import { Formik } from "formik";
+import * as yup from "yup";
 
 const initialValues = {
   username: "",
   password: "",
 };
+
+const singInValidationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Too short username (min 3 characters)")
+    .max(12, "Too long username (max 12 characters")
+    .required("Username is required"),
+  password: yup
+    .string()
+    .min(4, "Too short password (min 4 characters)")
+    .max(12, "Too long password (max 12 characters)")
+    .required("Password is required"),
+});
 
 const SignIn = () => {
   const onSubmit = (values) => {
@@ -12,7 +26,11 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={singInValidationSchema}
+    >
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
--- part10/rate-repository-app/src/components/TextInput.jsx
+++ part10/rate-repository-app/src/components/TextInput.jsx
@@ -1,9 +1,13 @@
 import { TextInput as NativeTextInput, StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  errorFrame: {
+    borderColor: "#d73a4a",
+  },
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = [style, error && styles.errorFrame];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
