fb0a46ddc3a441a961b67dda0acf9e27e046f53a
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-30 19:02:23 +0300

    add exercise 13.19

--- part13/models/index.js
+++ part13/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserBlogs = require('./user_blogs')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: UserBlogs, as: 'blog_readers' })
+Blog.belongsToMany(User, { through: UserBlogs, as: 'user_readinglist' })
 
 module.exports = {
   Blog,
